870c58f61dc5ad26c4f6a2956d361c10512515d4
vikram0602 <vikram0602@gmail.com>
2016-02-02 03:45:02 -0500

    first update

---
+++ Assets/scripts/enemy_health.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy_health : MonoBehaviour {
+    public float health = 100f;
+
+	public void removeHealth(float amount)
+    {
+        health -= amount;
+        if(health <=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
